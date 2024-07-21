56da23bb0b7b4d6fd6d5688b27c2cc14cd2cac90
sanidhya12345 <{sanidhya.varshney_cs19@gla.ac.in}+{sanidhya12345}@users.noreply.github.com>
2024-07-21 11:52:53 +0530

    update portfolio

--- app.py
+++ app.py
@@ -9,9 +9,18 @@ def hello_world():
 @app.route("/tools")
 def tools():
     return render_template("tools.html")
+
 @app.route("/certificates")
 def certificates():
     return render_template("certificates.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
 if __name__=="__main__":
     app.run(debug=True)
